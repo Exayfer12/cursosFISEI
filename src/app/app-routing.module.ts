734b80d1017a6af49158d24b5805d02b1108498f
David <jarcos4997@uta.edu.ec>
2023-01-04 00:41:14 -0500

    Arreglo de Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-//correguir import
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  //correguir import
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
