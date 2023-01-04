4a186e1f11a2bab8779df357cd84072f70321792
David <jarcos4997@uta.edu.ec>
2023-01-03 23:58:44 -0500

    Viendo errores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+//correguir import
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  //correguir import
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
