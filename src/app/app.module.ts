6d84e77b62c54f8bc5f3b2b138396fb3af7f4653
Exayfer <bmartinez8493@gmail.com>
2023-01-31 00:25:01 -0500

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/header/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { ListaCursosComponent } from './components/lista-cursos/lista-cursos.component';
+import { CursoItemComponent } from './components/curso-item/curso-item.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
+import { CarritoItemComponent } from './components/carrito-item/carrito-item.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    CursosComponent,
+    ListaCursosComponent,
+    CursoItemComponent,
+    CarritoComponent,
+    CarritoItemComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/curso-item/curso-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curso-item',
+  templateUrl: './curso-item.component.html',
+  styleUrls: ['./curso-item.component.css']
+})
+export class CursoItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
