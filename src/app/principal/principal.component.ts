34642c44b71c3d3004427514557ee03489c33c9b
AlexJmzc <73499509+AlexJmzc@users.noreply.github.com>
2023-01-31 14:46:21 -0500

    Listas para cards

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent implements OnInit {
+  data:any[] = [
+    {cedula:'12', nombre:'Jorge', apellido:'Delgado', cargo:'Gerente',correo: 'jorge@gmail.com'},
+    {cedula:'22', nombre:'Karla', apellido:'Martínez', cargo:'Contador',correo: 'karla@gmail.com'},
+    {cedula:'32', nombre:'Mishell', apellido:'Villegas', cargo:'Contador',correo: 'mishell@gmail.com'},
+    {cedula:'42', nombre:'Monserrath', apellido:'Olaya', cargo:'Gerente',correo: 'monse@gmail.com'},
+    {cedula:'52', nombre:'Alejandro', apellido:'Pazmiño', cargo:'Gerente',correo: 'alejandro@gmail.com'},
+  ]
+
+  ListFields: string[] = ['cedula','nombre','apellido','cargo','correo']
 
   constructor() { }
 
