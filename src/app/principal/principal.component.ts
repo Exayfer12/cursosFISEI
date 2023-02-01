b6175c9f9735b8bc3258599de6644432897954c3
AlexJmzc <73499509+AlexJmzc@users.noreply.github.com>
2023-02-01 01:17:39 -0500

    Componente de visualizacion de cursos

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent implements OnInit {
+  data:any[] = [
+    {nombre:'Conferencia 1', horas:'32', modalidad:'Presencial', descripcion:'Conferencia número 1', imagen:'https://img-c.udemycdn.com/course/480x270/980450_7fc0_4.jpg'},
+    {nombre:'Conferencia 2', horas:'32', modalidad:'Presencial', descripcion:'Conferencia número 2', imagen:'https://img-c.udemycdn.com/course/480x270/980450_7fc0_4.jpg'},
+    {nombre:'Curso 1', horas:'30', modalidad:'Virtual', descripcion:'Curso número 1', imagen:'https://img-c.udemycdn.com/course/480x270/980450_7fc0_4.jpg'},
+    {nombre:'Curso 2', horas:'20', modalidad:'Presencial', descripcion:'Curso número 2', imagen:'https://img-c.udemycdn.com/course/480x270/980450_7fc0_4.jpg'},
+    {nombre:'Curso 3', horas:'40', modalidad:'Virtual', descripcion:'Curso número 3', imagen:'https://img-c.udemycdn.com/course/480x270/980450_7fc0_4.jpg'},
+    {nombre:'Conferencia 3', horas:'32', modalidad:'Presencial', descripcion:'Conferencia número 3', imagen:'https://img-c.udemycdn.com/course/480x270/980450_7fc0_4.jpg'},
+    {nombre:'Conferencia 4', horas:'12', modalidad:'Virtual', descripcion:'Conferencia número 4', imagen:'https://img-c.udemycdn.com/course/480x270/980450_7fc0_4.jpg'},
+  ]
 
+  //ListFields: string[] = ['cedula','nombre','apellido','cargo','correo']
   constructor() { }
 
   ngOnInit(): void {
