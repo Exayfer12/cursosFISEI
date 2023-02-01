cdcc6416b300c6064305ca6283dfddb4d70285c2
Andresfl12350 <74079637+Andresfl12350@users.noreply.github.com>
2023-01-31 22:20:05 -0500

    Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InscripcionComponent } from './inscripcion/inscripcion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscripcionComponent
   ],
   imports: [
     BrowserModule,
